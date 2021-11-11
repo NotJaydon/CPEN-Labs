caddeffc996dbe81c6ce810766b8b71b2beffbe2
Jaydon Alexis <jjalexis@student.ubc.ca>
2021-11-11 15:47:29 -0800

    Added shifter file

--- alu.v
+++ alu.v
@@ -8,7 +8,17 @@ module ALU(Ain, Bin, ALUop, out, Z);
 	reg Z;
 	
 	always @* begin
-		
-	end
+		case(ALUop)
+			2'b00: out = Ain + Bin;
+			2'b01: out = Ain - Bin;
+			2'b10: out = Ain & Bin;
+			2'b11: out = ~(Bin);
+			default: out = ({16{1'bx}});
+		endcase
 
+		if (out == {16{1'b0}})	//issues with concatenation for some reason, can't be bothered
+			Z = 1'b1;
+		else
+			Z = 1'b0;
+	end
 endmodule
