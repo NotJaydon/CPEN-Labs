1ed412229d49127bc968320b819a05ed0e1eb9f2
Jaydon Alexis <jjalexis@student.ubc.ca>
2021-11-11 09:59:36 -0800

    Added more files + test

--- regfile.v
+++ regfile.v
@@ -2,6 +2,6 @@ module regfile(data_in, writenum, write, readnum, clk, data_out);
 	input [15:0] data_in;
 	input [2:0] writenum, readnum;
 	input wire, clk;
-	output [15:0] data_out; 
+	output [15:0] data_out;
 
 endmodule
