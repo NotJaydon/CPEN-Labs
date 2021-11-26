93ff30272c07409b4534443fac986221353e7d3a
Jaydon Alexis <jjalexis@student.ubc.ca>
2021-11-25 20:35:30 -0800

    update

--- controller_fsm.v
+++ controller_fsm.v
@@ -1,31 +1,45 @@
-module controller_fsm(clk, s, reset, opcode, op, w, nsel, vsel, write, loada, loadb, asel, bsel, loadc, loads);
-input s, reset, clk;
-input [1:0] op;
+module controller_fsm(mem_cmd, load_pc, load_ir, reset_pc, addr_sel, clk, reset, opcode, op, nsel, vsel, write, loada, loadb, asel, bsel, loadc, loads);
 input [2:0] opcode;
+input [1:0] op;
+input reset, clk;
+//need to add additional outputs - resolved
 
-output w, write, loada, loadb, asel, bsel, loadc, loads;
+//compiler doesn't pick up width errors - need to check
+
 output [3:0] vsel;
-output [2:0] nsel;
+output [2:0] nsel;	//specified in the pdf that nsel was 2 bits wide but I think we can leave it as is
+output [1:0] mem_cmd;
+output write, loada, loadb, asel, bsel, loadc, loads, load_pc, load_ir, reset_pc, addr_sel;
 
-`define wait_ 4'b0000
-`define decode 4'b0001
-`define geta 4'b1000 
-`define getb 4'b1001
-`define arithmetic 4'b1010
-`define writereg 4'b1110
-`define movimm 4'b0100
-`define movreguno 4'b0101
-`define movregdos 4'b0110
-`define movregtres 4'b0111
-`define compare 4'b0010
+//Use these definitions for M commands in lab 7 top
+`define MNONE 2'b00
+`define MREAD 2'b01
+`define MWRITE 2'b11
+
+`define RST 4'b0000
+`define IF1 4'b0001
+`define IF2 4'b0010
+`define UpdatePC 4'b0011
+`define decode 4'b0100
+`define geta 4'b0101
+`define getb 4'b0110
+`define arithmetic 4'b0111
+`define writereg 4'b1000
+`define movimm 4'b1001
+`define movreguno 4'b1010
+`define movregdos 4'b1011
+`define movregtres 4'b1100
+`define compare 4'b1101
+
+//Make states for MNONE, MREAD, MWRITE - resolved
 
 wire [3:0] present_state, state_next_reset, state_next;
-reg [18:0] next;
+reg [23:0] next;	//change the width of next - resolved
 
 vDFF #(4) STATE(.clk(clk), .in(state_next_reset), .out(present_state)); //simple flipflop module to change 
 									//states on risedge of clock
 
-assign state_next_reset = reset ? `wait_ : state_next; //checks to see if reset is high (in which case we go back to beginning)
+assign state_next_reset = reset ? `RST : state_next; //checks to see if reset is high (in which case we go back to beginning)
 
 //vsel - will not do anything if write is 0
 //nsel - will not do anything if write is 0 and also if load a and load b are zero
@@ -38,34 +52,36 @@ assign state_next_reset = reset ? `wait_ : state_next; //checks to see if reset 
 //w - needs to be 1 or 0 and is local to this current module
 
 always @* begin	//continuously updates present state and outputs based on the past state and inputs
-		//state, vsel, nsel, write, loadb, loada, asel, bsel, loadc, loads, w
-   casex({present_state, op, opcode, s}) //checking state and inputs
-	{`wait_, 2'bxx, 3'bxxx, 1'b1}: next <= {`decode, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`wait_, 2'bxx, 3'bxxx, 1'b0}: next <= {`wait_, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b1};
-	{`decode, 2'bxx, 3'b101, 1'bx}: next <= {`geta, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`decode, 2'b10, 3'b110, 1'bx}: next <= {`movimm, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`decode, 2'b00, 3'b110, 1'bx}: next <= {`movreguno, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
+		//state, vsel, nsel, write, loadb, loada, asel, bsel, loadc, loads, reset_pc, load_pc, addr_sel, mem_cmd, load_ir
+   casex({present_state, op, opcode}) //checking state and inputs
+	{`RST, 2'bxx, 3'bxxx}: next <= {`IF1, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b1, 1'b1, 1'bx, `MNONE, 1'b0};	//Not sure but for now, addr_sel is 1'bx, could be 0 as shown in diagram
+	{`IF1, 2'bxx, 3'bxxx}: next <= {`IF2, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, `MREAD, 1'b0};	//addr_sel state NOT RESOLVED
+	{`IF2, 2'bxx, 3'bxxx}: next <= {`UpdatePC, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, `MREAD, 1'b1};
+	{`UpdatePC, 2'bxx, 3'bxxx}: next <= {`decode, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b1, 1'bx, `MNONE, 1'b0};
+	{`decode, 2'bxx, 3'b101}: next <= {`geta, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`decode, 2'b10, 3'b110}: next <= {`movimm, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`decode, 2'b00, 3'b110}: next <= {`movreguno, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
 	
-	{`geta, 2'bxx, 3'bxxx, 1'bx}: next <= {`getb, 4'bxxxx, 3'b100, 1'b0, 1'b0, 1'b1, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`getb, 2'b00, 3'bxxx, 1'bx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`getb, 2'b10, 3'bxxx, 1'bx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`getb, 2'b11, 3'bxxx, 1'bx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`getb, 2'b01, 3'bxxx, 1'bx}: next <= {`compare, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`arithmetic, 2'bxx, 3'bxxx, 1'bx}: next <= {`writereg, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 1'b1, 1'b0};
-	{`writereg, 2'bxx, 3'bxxx, 1'bx}: next <= {`wait_, 4'b0001, 3'b010, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
+	{`geta, 2'bxx, 3'bxxx}: next <= {`getb, 4'bxxxx, 3'b100, 1'b0, 1'b0, 1'b1, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`getb, 2'b00, 3'bxxx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`getb, 2'b10, 3'bxxx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`getb, 2'b11, 3'bxxx}: next <= {`arithmetic, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`getb, 2'b01, 3'bxxx}: next <= {`compare, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`arithmetic, 2'bxx, 3'bxxx}: next <= {`writereg, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 1'b1, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`writereg, 2'bxx, 3'bxxx}: next <= {`IF1, 4'b0001, 3'b010, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};	//not sure aobut IF1
 	
-	{`movimm, 2'bxx, 3'bxxx, 1'bx}: next <= {`wait_, 4'b0100, 3'b100, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
+	{`movimm, 2'bxx, 3'bxxx}: next <= {`IF1, 4'b0100, 3'b100, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};	//not sure aobut IF1
 	
-	{`movreguno, 2'bxx, 3'bxxx, 1'bx}: next <= {`movregdos, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
-	{`movregdos, 2'bxx, 3'bxxx, 1'bx}: next <= {`movregtres, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b1, 1'b0, 1'b0};
-	{`movregtres, 2'bxx, 3'bxxx, 1'bx}: next <= {`wait_, 4'b0001, 3'b010, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0};
+	{`movreguno, 2'bxx, 3'bxxx}: next <= {`movregdos, 4'bxxxx, 3'b001, 1'b0, 1'b1, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`movregdos, 2'bxx, 3'bxxx}: next <= {`movregtres, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b1, 1'b0, 1'b1, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};
+	{`movregtres, 2'bxx, 3'bxxx}: next <= {`IF1, 4'b0001, 3'b010, 1'b1, 1'b0, 1'b0, 1'bx, 1'bx, 1'b0, 1'b0, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};	//not sure aobut IF1
 
-	{`compare, 2'bxx, 3'bxx, 1'bx}: next <= {`wait_, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 1'b1, 1'b0};
-	default: next = {19{1'bx}};
+	{`compare, 2'bxx, 3'bxxx}: next <= {`IF1, 4'bxxxx, 3'bxxx, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b1, 1'b1, 1'b0, 1'b0, 1'bx, `MNONE, 1'b0};	//not sure aobut IF1
+	default: next = {24{1'bx}};
     endcase
 end
 
-assign {state_next, vsel, nsel, write, loadb, loada, asel, bsel, loadc, loads, w} = next;
+assign {state_next, vsel, nsel, write, loadb, loada, asel, bsel, loadc, loads, reset_pc, load_pc, addr_sel, mem_cmd, load_ir} = next;
 
 endmodule
 		
